a30e6888ff1b3cb17cee7051b2bc4dfa54520010
minjunine <rnalswn253@naver.com>
2024-08-21 17:27:08 +0900

    first commit

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Senicare.css';
-import Auth from './views/Auth';
+import Auth from '<source />/views/Auth';
 
 export default function Senicare() {
   return (
-    <></>
+    <>
+      <Auth />
+    </>
   );
 }
