dd0bd28fc46cbade6202e7cd80142d3d939c851f
minjunine <rnalswn253@naver.com>
2024-09-05 11:54:22 +0900

    first commit

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,21 +1,52 @@
-import { Outlet } from 'react-router'
-import './style.css'
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { Outlet, useLocation, useNavigate } from 'react-router';
+import './style.css';
 
 // component: 로고 컴포넌트 //
 function Logo() {
 
   // render: 로고 컴포넌트 렌더링 //
   return (
-    <div id='layout-log'></div>
+    <div id='layout-log'>
+      <div className='logo-box'>
+        <div className='title'>시니케어</div>
+        <div className='icon'></div>
+      </div>
+    </div>
   )
 }
 
 // component: 상단 컴포넌트 //
 function Top() {
 
+  // state: path 상태 //
+  const { pathname } = useLocation();
+  // state: cookie 상태 //
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  // variable: 경로 이름 //
+  const path = 
+      pathname.startsWith('/cs') ? '고객 관리' :
+      pathname.startsWith('/mm') ? '용품 관리' :
+      pathname.startsWith('/hr') ? '인사 관리' : '';
+  
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // event handler : 로그아웃 버튼 클릭 이벤트 처리 //
+  const onLogoutButtonClickHandler = () => {
+    removeCookie('accessToken', {path: '/'});
+    navigator('/auth');
+  };
+
   // render: 상단 컴포넌트 //
   return (
-    <div id='layout-top'></div>
+    <div id='layout-top'>
+      <div className='path'>{path}</div>
+      <div className='button second' onClick={onLogoutButtonClickHandler}>로그아웃</div>
+    </div>
+
   )
 }
 
@@ -30,6 +61,16 @@ function SideNavigation() {
 
 // component: 메인 레이아웃 컴포넌트 //
 export default function MainLayout() {
+
+  // state: 쿠키 상태 //
+  const [cookies] = useCookies();
+
+  // function : 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // effect: 레이아웃 마운트시 로그인 여부 확인 //
+  useEffect(() => {
+  }, []);
 
     // render: 메인 레이아웃 컴포넌트 렌더링 //
     return (
